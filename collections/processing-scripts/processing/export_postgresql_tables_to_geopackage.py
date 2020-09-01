646315d92689df8e7c33aede3d422b371a31bb06
Michael Douchin <mdouchin@3liz.com>
2020-09-01 15:44:19 +0200

    New alg - Export PostgreSQL tables to GeoPackages

---
+++ collections/processing-scripts/processing/export_postgresql_tables_to_geopackage.py
@@ -0,0 +1,161 @@
+import os
+from qgis.PyQt.QtCore import QCoreApplication
+from qgis.core import (
+    QgsProcessingAlgorithm,
+    QgsProcessingException,
+    QgsProcessingParameterString,
+    QgsProcessingParameterCrs,
+    QgsDataSourceUri,
+    QgsProcessingParameterFileDestination,
+)
+from processing.algs.gdal.GdalUtils import GdalUtils
+from processing.tools.postgis import uri_from_name
+
+class ExportPostgresqlTablesToGeopackage(QgsProcessingAlgorithm):
+    """
+    Export tables from postgreSQL listed schemas to geopackage file
+    """
+
+    CONNECTION_NAME = 'CONNECTION_NAME'
+    SCHEMAS = 'SCHEMAS'
+    TABLES_BLOCK_LIST = 'TABLE_BLOCK_LIST'
+    DESTINATION = 'DESTINATION'
+
+    @staticmethod
+    def tr(string):
+        return QCoreApplication.translate('Processing', string)
+
+    def createInstance(self):
+        return ExportPostgresqlTablesToGeopackage()
+
+    def name(self):
+        return 'export_postgresql_tables_to_geopackage'
+
+    def displayName(self):
+        return self.tr('Export PostgreSQL tables to a Geopackage file')
+
+    def group(self):
+        return self.tr('Vector')
+
+    def groupId(self):
+        return 'vector'
+
+    def shortHelpString(self):
+        return self.tr("Choose a PostgreSQL connection, give a list of schemas separated by comma, and the destination GeoPackage file to create")
+
+    def initAlgorithm(self, config=None):
+        """
+        Here we define the inputs and output of the algorithm, along
+        with some other properties.
+        """
+        # INPUTS
+        db_param = QgsProcessingParameterString(
+            self.CONNECTION_NAME,
+            self.tr('PostgreSQL connection'),
+            optional=False
+        )
+        db_param.setMetadata({
+            'widget_wrapper': {
+                'class': 'processing.gui.wrappers_postgis.ConnectionWidgetWrapper'
+            }
+        })
+        self.addParameter(db_param)
+
+        schemas_param = QgsProcessingParameterString(
+            self.SCHEMAS,
+            self.tr('List of schemas, separated by commas'),
+            optional=False
+        )
+        self.addParameter(schemas_param )
+
+        self.addParameter(
+            QgsProcessingParameterString(
+                self.TABLES_BLOCK_LIST,
+                self.tr('List of tables to NOT export, separated by commas'),
+                defaultValue='public.spatial_ref_sys',
+                optional=True
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterFileDestination(
+                self.DESTINATION,
+                'GeoPackage file',
+                fileFilter='gpkg'
+            )
+        )
+
+    def getPostgisConnectionUriFromName(self, connection_name):
+        """
+        Return a QgsDatasourceUri from a PostgreSQL connection name
+        """
+
+        uri = uri_from_name(connection_name)
+
+        # In QGIS 3.10, we will use
+        # metadata = QgsProviderRegistry.instance().providerMetadata('postgres')
+        # find a connection by name
+        # connection = metadata.findConnection(connection_name)
+        # uri_str = connection.uri()
+        # uri = QgsDataSourceUri(uri)
+
+        return uri
+
+    def processAlgorithm(self, parameters, context, feedback):
+        """
+        Here is where the processing itself takes place.
+        """
+        connection_name = parameters[self.CONNECTION_NAME]
+        schemas = self.parameterAsString(parameters, self.SCHEMAS, context)
+        schemas = ','.join([s.strip() for s in schemas.split(',') ])
+        output_path = self.parameterAsString(parameters, self.DESTINATION, context)
+        if not output_path.lower().endswith('.gpkg'):
+            output_path += '.gpkg'
+
+        if os.path.exists(output_path):
+            os.remove(output_path)
+            feedback.pushDebugInfo(self.tr('Previous Geopackage has been deleted !'))
+
+        uri = self.getPostgisConnectionUriFromName(connection_name)
+        p = context.project()
+        if uri.service():
+            ogr_source = 'PG:service=%s schemas=%s ' % (
+                pg_service,
+                schemas
+            )
+        else:
+            ogr_source = 'PG:host=%s port=%s dbname=%s user=%s password=%s schemas=%s ' % (
+                uri.host(),
+                uri.port(),
+                uri.database(),
+                uri.username(),
+                uri.password(),
+                schemas
+            )
+
+        ogr_arguments = [
+            '',
+            '-overwrite',
+            '-progress',
+            '-f', 'GPKG',
+            output_path,
+            ogr_source,
+            '-lco', 'GEOMETRY_NAME=geom',
+            '-lco', 'SPATIAL_INDEX=YES',
+            '-gt', '50000',
+            '--config', 'PG_LIST_ALL_TABLES', 'YES',
+            '--config', 'PG_SKIP_VIEWS', 'YES',
+            '--config', 'OGR_SQLITE_SYNCHRONOUS', 'OFF',
+            '--config', 'OGR_SQLITE_CACHE', '1024'
+        ]
+
+
+        feedback.pushInfo('OGR command = ogr2ogr {}'.format(' '.join(ogr_arguments)))
+        GdalUtils.runGdal(['ogr2ogr', GdalUtils.escapeAndJoin(ogr_arguments)], feedback)
+
+        if not os.path.isfile(output_path):
+            raise QgsProcessingException(self.tr('GeoPackage has not been sucessfully created.'))
+
+        feedback.pushInfo(self.tr('Geopackage sucessfull created: "{}"'.format(output_path)))
+
+        return {}
